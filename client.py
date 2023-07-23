52f678e14ba310020da1b0dec55ace7b443d1d76
FahrilRefiandi <fahril.refandi@gmail.com>
2023-07-23 20:36:18 +0700

    update

---
+++ client.py
@@ -0,0 +1,76 @@
+import socket
+import threading
+import os
+from PIL import Image
+import base64
+
+def receive_data(sock):
+    while True:
+        data = sock.recv(1024).decode()
+        print(data)
+
+def send_image(sock, recipient, img_path):
+    img_name = os.path.basename(img_path)
+    with open(img_path, "rb") as f:
+        img_data = f.read()
+    img_data_b64 = base64.b64encode(img_data).decode()
+    message = f"{recipient}|image|{img_name}|{img_data_b64}"
+    sock.send(message.encode())
+
+def send_video(sock, recipient, video_path):
+    video_name = os.path.basename(video_path)
+    with open(video_path, "rb") as f:
+        video_data = f.read()
+    video_data_b64 = base64.b64encode(video_data).decode()
+    message = f"{recipient}|video|{video_name}|{video_data_b64}"
+    sock.send(message.encode())
+
+def main():
+    host = 'localhost'
+    port = 12345
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    username = input("Enter your username: ")
+    client_socket.send(username.encode())
+
+    receive_thread = threading.Thread(target=receive_data, args=(client_socket,))
+    receive_thread.start()
+
+    while True:
+        print("\nMenu:")
+        print("1. Send Message")
+        print("2. Send Image")
+        print("3. Send Video")
+        print("4. Exit")
+
+        choice = input("Your choice: ")
+
+        if choice == "1":
+            recipient = input("Enter recipient's username: ")
+            message = input("Enter your message: ")
+            client_socket.send(f"{recipient}|message|{message}".encode())
+
+        elif choice == "2":
+            recipient = input("Enter recipient's username: ")
+            img_path = input("Enter the image file path: ")
+            if os.path.exists(img_path):
+                send_image(client_socket, recipient, img_path)
+            else:
+                print("File not found!")
+
+        elif choice == "3":
+            recipient = input("Enter recipient's username: ")
+            video_path = input("Enter the video file path: ")
+            if os.path.exists(video_path):
+                send_video(client_socket, recipient, video_path)
+            else:
+                print("File not found!")
+
+        elif choice == "4":
+            client_socket.close()
+            exit()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,5 +1,79 @@
 import socket
 import threading
+import os
 
+def handle_client(client_socket, client_address, clients):
+    username = client_socket.recv(1024).decode()
+    clients[username] = client_socket
 
+    print(f"[INFO] {username} connected from {client_address}")
 
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+
+        message = data.decode()
+        recipient, msg_type, content = message.split("|")
+
+        if msg_type == "message":
+            print(f"[{username} -> {recipient}]: {content}")
+            if recipient in clients:
+                recipient_socket = clients[recipient]
+                recipient_socket.send(f"[{username}]: {content}".encode())
+            else:
+                client_socket.send(f"Recipient '{recipient}' not found.".encode())
+
+        elif msg_type == "image":
+            img_name, img_data = content.split("|")
+            save_image(img_name, img_data)
+
+            print(f"[{username} -> {recipient}]: {img_name} (Image)")
+            if recipient in clients:
+                recipient_socket = clients[recipient]
+                recipient_socket.send(f"[{username}] sent an image: {img_name}".encode())
+            else:
+                client_socket.send(f"Recipient '{recipient}' not found.".encode())
+
+        elif msg_type == "video":
+            video_name, video_data = content.split("|")
+            save_video(video_name, video_data)
+
+            print(f"[{username} -> {recipient}]: {video_name} (Video)")
+            if recipient in clients:
+                recipient_socket = clients[recipient]
+                recipient_socket.send(f"[{username}] sent a video: {video_name}".encode())
+            else:
+                client_socket.send(f"Recipient '{recipient}' not found.".encode())
+
+    del clients[username]
+    client_socket.close()
+    print(f"[INFO] {username} disconnected.")
+
+def save_image(img_name, img_data):
+    with open(f"received_{img_name}", "wb") as f:
+        f.write(img_data.encode())
+
+def save_video(video_name, video_data):
+    with open(f"received_{video_name}", "wb") as f:
+        f.write(video_data.encode())
+
+def main():
+    host = '0.0.0.0'
+    port = 12345
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(5)
+
+    print("[INFO] Server listening on port", port)
+
+    clients = {}
+
+    while True:
+        client_socket, client_address = server.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address, clients))
+        client_thread.start()
+
+if __name__ == "__main__":
+    main()
