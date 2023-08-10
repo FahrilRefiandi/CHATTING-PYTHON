68bc2bdc9ecb37108ffd72de59fba3254a0e2150
FahrilRefiandi <fahril.refandi@gmail.com>
2023-08-10 09:04:11 +0700

    hapus file yg tidak terpakai

--- percobaan/client2.py
+++ percobaan/client2.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import os
-import base64
+
 
 
 
@@ -20,24 +20,22 @@ def receive_data(sock):
             
     except Exception as e:
         print("Error:", e)
+    buffer = b""
 
 def process_message(message):
     
     parts = message.split(b"||")
     
-    
-    
-
     sender = parts[0].decode()
     msg_type = parts[2].decode()
     content = parts[1]
 
     if msg_type == "message":
         print(f"\n{sender}: {content.decode()}")
-    elif msg_type == "image":
+    elif msg_type == "image" or msg_type == "video" or msg_type == "file":
         file_path = parts[3].decode()
         file_name = os.path.basename(file_path)
-        
+        content = content.decode()
         
         generateFile(file_name, content)
         print(f"\n{sender}: {file_name}")
@@ -48,30 +46,27 @@ def generateFile(file_name, file_data):
         file_data = file_data.encode()
     
     
-    if file_data.startswith(b"b'") or file_data.startswith(b'b"'):
-        file_data = file_data[2:-1]
-    
-    
-    try:
-        decoded_data = base64.b64decode(file_data)
-    except:
-        decoded_data = file_data
-    
     os.makedirs('ASSET', exist_ok=True)
     
-    file_path = os.path.join('ASSET', file_name)
     
+    
+    path=os.path.join('ASSET', file_name)
+    print(path)
     
 
-    with open(os.path.join('ASSET', file_name), 'wb') as destination_file:
-        destination_file.write(decoded_data)
+    with open(path, 'wb') as destination_file:
+        destination_file.write(file_data)
 
     
-    return destination_file.name
+
+
+    
+    return path
 
 def main():
 
-    host = '192.168.1.26'
+    # host = 'localhost'
+    host = '10.217.19.120'
     port = 12345
     
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -88,7 +83,8 @@ def main():
         print("1. Send Message")
         print("2. Send Image")
         print("3. Send Video")
-        print("4. Exit")
+        print("4. Send File")
+        print("5. Exit")
 
         choice = input("Your choice: ")
 
@@ -112,8 +108,16 @@ def main():
                 send_file(client_socket, recipient, vid_path, "video")
             else:
                 print("File not found!")
-
+                
         elif choice == "4":
+            recipient = input("Enter recipient's username ('all',[username1,username2] for all users): ")
+            vidPath = input("Enter the video file path: ")
+            if os.path.exists(vidPath):
+                send_file(client_socket, recipient, vidPath, "file")
+            else:
+                print("File not found!")
+
+        elif choice == "5":
             client_socket.close()
             break
 
@@ -124,16 +128,13 @@ def send_message(sock, recipient, message):
     message = f"{recipient}||message||{message}||null"
     sock.send(message.encode())
 
-# def send_file(sock, recipient, file_path, msg_type):
-#     message = f"{recipient}|{msg_type}|{file_path}"
-#     sock.send(message.encode())
-
 def send_file(sock, recipient, file_path, msg_type):
-     with open(file_path, 'rb') as file:
+    with open(file_path, 'rb') as file:
         image_data = file.read()
-     message = f"{recipient}||{msg_type}||{image_data}||{file_path}"
-    #  print(message)
-     sock.send(message.encode())
+     
+
+    message = f"{recipient}||{msg_type}||{image_data}||{file_path}"
+    sock.send(message.encode())
 
 
 if __name__ == "__main__":
--- percobaan/server.py
+++ percobaan/server.py
@@ -1,11 +1,11 @@
 import socket
 import threading
-import base64
-import os
+
 
 
 def main():
-    host = '192.168.1.26'
+    # host = 'localhost'
+    host = '10.217.19.120'
     port = 12345
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -36,10 +36,6 @@ def handle_client(client_socket, client_address, clients):
 
             file_data += data
 
-            
-            # print(file_data)
-
-            # Process only if the data contains at least two '|' characters
             if file_data.count(b"||") < 2:
                 continue
 
@@ -62,36 +58,27 @@ def handle_client(client_socket, client_address, clients):
             msg_type = rest_data.split(b"||", 1)[0]
             content = rest_data.split(b"||", 2)[1]
             
-            # get file path from last parameter file_data sampai | terakhir
             file_path = rest_data.split(b"||")[-1]
-            
-            
             
             
 
             if msg_type == b"message":
                 content = content.strip().decode()
-                # hilangkan b'' pada msg_type
                 msg_type = msg_type.decode()
 
                 send_message(clients, username, recipient, content,msg_type,file_path)
-            elif msg_type == b"image" or msg_type == b"video":
+            elif msg_type == b"image" or msg_type == b"video" or msg_type == b"file":
                 content = content.decode()
                 msg_type = msg_type.decode()
                 send_file(clients, username, recipient, content, msg_type, file_path)
-            # else:
-                # print(msg_type)
-                # print("Invalid message type!")
-
+            
         except OSError:
             break
 
-        # Clear the file data before the next iteration
-        file_data = b""
+    file_data = b""
 
 
 def send_message(clients,username,recipient,content,msg_type,file_path):
-    # print(username, content, msg_type)
     for rcpnt in recipient:
         if rcpnt in clients.keys():
             if rcpnt == username:
@@ -101,22 +88,9 @@ def send_message(clients,username,recipient,content,msg_type,file_path):
         else:
             clients[rcpnt].send(f"{username}||{content}||{msg_type}".encode())
     
-    # for rcpnt in recipient:
-    #     if rcpnt in clients:
-    #         message = f"{username}||{content}||{msg_type}".encode()
-    #         clients[rcpnt].send(message)
-    #     else:
-    #         print(f"Recipient '{rcpnt}' not found in clients dictionary")
-
-        
 
 
 def send_file(clients, username, recipient, content, msg_type, file_path):
-    print(content)
-    content = content.encode('utf-8')
-    content = base64.b64encode(content).decode('utf-8')
-    
-
     for rcpnt in recipient:
         if rcpnt in clients.keys():
             if rcpnt == username:
@@ -125,13 +99,7 @@ def send_file(clients, username, recipient, content, msg_type, file_path):
                 clients[rcpnt].send(f"{username}||{content}||{msg_type}||{file_path}".encode())
         else:
             clients[username].send(f"Recipient '{rcpnt}' not found.".encode())
-    # for rcpnt in recipient:
-    #     if rcpnt in clients:
-    #         message = f"{username}||{content}||{msg_type}".encode()
-    #         clients[rcpnt].send(message)
-    #     else:
-    #         print(f"Recipient '{rcpnt}' not found in clients dictionary")
-
+    
  
 if __name__ == "__main__":
     main()
